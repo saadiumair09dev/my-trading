a9614c1875e211e30918caed176c90b595f24d8b
saadiumair09dev <saadiumair09@gmail.com>
2026-04-15 18:19:49 +0530

    Add setup.sh script for Dhan API integration

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install dhanhq package
+pip install dhanhq
+
+# Set up environment variables for Dhan API integration
+export DHAN_API_KEY='your_api_key'
+export DHAN_API_SECRET='your_api_secret'
+
+echo "Dhan API integration setup complete."
